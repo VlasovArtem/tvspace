f13661249f38cd03dbe579ec6fc3a6a59ab6adba
Vlasov Artem <avlasov@selectica.com>
2016-01-12 11:26:53 +0200

    Add UserServiceTest, update service impl and service functions

---
+++ src/test/java/com/vlasovartem/tvspace/service/UserServiceTest.java
@@ -0,0 +1,20 @@
+package com.vlasovartem.tvspace.service;
+
+import com.vlasovartem.tvspace.persistence.repository.UserRepository;
+import org.easymock.Mock;
+import org.easymock.TestSubject;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by artemvlasov on 14/12/15.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class UserServiceTest {
+
+    @TestSubject
+    private UserService
+
+    @Mock
+    private UserRepository userRepository;
+}
